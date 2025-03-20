2d1a00a584f600cf10bc1ae8dbccb45ec17a5156
lubnaNoor0210 <167392315+lubnaNoor0210@users.noreply.github.com>
2025-03-20 09:27:25 +0500

    Add files via upload

--- Password Generator/main.py
+++ Password Generator/main.py
@@ -1,1 +1,18 @@
+import streamlit as st
+import random
+import string
 
+
+st.title("🔑Simple Password Generator🔐")
+
+
+length = st.number_input("Enter password length:", min_value=4, max_value=50, step=1)
+
+
+if st.button("Generate Password"):
+    
+    characters = string.ascii_letters + string.digits + string.punctuation
+    
+    password = ''.join(random.choice(characters) for _ in range(length))
+    
+    st.success(f"Your generated password is: {password}")
