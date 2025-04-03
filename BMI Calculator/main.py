00f86e1e8039aca1d4f1e7b57c26b6e99246fe2d
lubnaNoor0210 <167392315+lubnaNoor0210@users.noreply.github.com>
2025-04-03 20:48:56 +0500

    Update main.py

--- BMI Calculator/main.py
+++ BMI Calculator/main.py
@@ -1,30 +1,28 @@
-# import streamlit as st
-# 
-# st.title("BMI Calculator")
-# weight = st.number_input("Enter your weight (kg):", min_value=1.0 , step=1.0)
-# height = st.number_input("Enter your height (m):" , min_value=0.1 , step= 0.01)
-# 
-# def calculate_bmi(weight , height):
-#     if height > 0:
-#         return weight / (height ** 2)
-#     else:
-#         return None
-# 
-# if st.button("Calculate BMI"):
-#     bmi = calculate_bmi(weight , height)
-#     if bmi:
-#         st.success(f"Your BMI is: {bmi:.2f}")
-#         if bmi < 18.5:
-#             st.warning("You are underweight.")
-#         elif 18.5 <= bmi < 35.9:
-#             st.success("Your Weight Is Normal")
-#         elif 25 <= bmi < 40.9:
-#             st.warning("You are overweight.")
-#         else:
-#             st.error("You are obese.")
-#     else:
-#         st.error("Please enter a valid height.")
-# 
-# 
-# 
-#
+import streamlit as st
+
+st.title("BMI Calculator")
+weight = st.number_input("Enter your weight (kg):", min_value=1.0 , step=1.0)
+height = st.number_input("Enter your height (m):" , min_value=0.1 , step= 0.01)
+
+def calculate_bmi(weight , height):
+    if height > 0:
+        return weight / (height ** 2)
+    else:
+        return None
+    
+if st.button("Calculate BMI"):
+    bmi = calculate_bmi(weight , height)
+    if bmi:
+        st.success(f"Your BMI is: {bmi:.2f}")
+        if bmi < 18.5:
+            st.warning("You are underweight.")
+        elif 18.5 <= bmi < 35.9:
+            st.success("Your Weight Is Normal")
+        elif 25 <= bmi < 40.9:
+            st.warning("You are overweight.")
+        else:
+            st.error("You are obese.")
+    else:
+        st.error("Please enter a valid height.")
+
+
